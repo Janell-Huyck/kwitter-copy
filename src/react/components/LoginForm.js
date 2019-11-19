51e65ee04b57cd3c89ec2e2b51629c8f762cff50
givond4 <Givond4@gmail.com>
2019-11-19 17:01:23 -0500

    small changes

--- src/react/components/LoginForm.js
+++ src/react/components/LoginForm.js
@@ -3,7 +3,7 @@ import { Spinner } from ".";
 import { withAsyncAction } from "../HOCs";
 import "./LoginForm.css";
 import { Input, Button } from "semantic-ui-react";
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 class LoginForm extends React.Component {
   state = { username: "", password: "" };
@@ -50,7 +50,7 @@ class LoginForm extends React.Component {
             </Button>
             <button className="createAccountButton" type="submit">
               Create Account
-            </Link>
+            </button>
             
           </form>
           {loading && <Spinner name="circle" color="blue" />}
