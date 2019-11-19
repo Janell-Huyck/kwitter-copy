0e432a7b8b511dbca246207531d137f2ba2d0ed4
Jordan Davidson <jordandavidson744@yahoo.com>
2019-11-19 14:44:08 -0500

    working on creating an account

---
+++ src/react/pages/createAccount.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class createAccount extends Component {
+    render() {
+        return (
+            <>
+            
+            </>
+        )
+    }
+}
