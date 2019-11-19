ebc1373108e11ea849560a54e0e48959120a7504
Jordan Davidson <jordandavidson744@yahoo.com>
2019-11-19 11:52:29 -0500

    updated profile card

--- src/react/components/ProfileCard.js
+++ src/react/components/ProfileCard.js
@@ -5,12 +5,18 @@ const fakeUser = {
     pictureLocation: 'https://boygeniusreport.files.wordpress.com/2016/11/puppy-dog.jpg?quality=98&strip=all&w=782',
     username: 'jordans-Test',
     displayName: 'Jordan\'s Test',
-    about: 'testing bio',
+    about: '',
     googleId: null,
-    createdAt: '',
+    createdAt: '2019-11-18T15:10:16.100Z',
     updatedAt: ''
 };
-const bio = 'bio: '
+    const month = fakeUser.createdAt.split('-').splice(1, 1)
+    const year = fakeUser.createdAt.split('-').splice(0,1)
+    const day = fakeUser.createdAt.slice(0, fakeUser.createdAt.indexOf('T')).split('-').reverse().shift()
+    const date = month + '-' + day + '-' + year
+    
+    const date2 = fakeUser.createdAt.slice(0, fakeUser.createdAt.indexOf('T')).split('-').reverse()
+    console.log(date2)
 class ProfileCard extends Component {
     render() {
         return (
@@ -21,7 +27,8 @@ class ProfileCard extends Component {
                     </div>
                     <h2>{fakeUser.displayName}</h2>
                     <p>{fakeUser.username}</p>
-                    <p id='userBio'>{fakeUser.about && bio + fakeUser.about}</p>
+                    <p>joined: {date}</p>
+                    <p id='userBio'>{fakeUser.about ? 'bio: ' + fakeUser.about : 'No bio provided by this user'}</p>
                 </div>
             </>
         )
