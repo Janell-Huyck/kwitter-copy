d30bf678ceaabb4fc344e762a61f6f070c81dfac
Janell Huyck <janell.huyck@outlook.com>
2019-11-20 11:37:21 -0500

    initial branch commit

---
+++ src/react/components/MessageCard.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+new (class MessageCard extends React.Component {
+  render() {
+    return <div>message card component</div>;
+  }
+})();
+
+export default MessageCard;
