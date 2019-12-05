2dc66a21baf5393085fe7744f6aa449cacbe6afc
Janell Huyck <janell.huyck@outlook.com>
2019-12-05 09:42:52 -0500

    capitalizing E

--- src/react/components/index.js
+++ src/react/components/index.js
@@ -7,9 +7,9 @@ export { default as NewMessageBox } from "./NewMessageBox";
 export { default as MessageList } from "./MessageList";
 export { default as CreatedAt } from "./CreatedAt";
 export { default as DeleteUserButton } from "./DeleteUserButton";
-export { default as EditUserForm } from "./EditUserForm";
 export { default as DeleteMessage } from "./DeleteMessage";
 export { default as UploadUserPicture } from "./UploadUserPicture";
+export { default as EditUserForm } from "./EditUserForm";
 
 // components from other packages
 export { Switch, Route, Link, NavLink } from "react-router-dom";
